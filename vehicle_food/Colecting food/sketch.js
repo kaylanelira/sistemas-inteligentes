1641a8faac6a38f279846ecbc0d2de25face6879
Kaylane Lira <kgl@cin.ufpe.br>
2023-06-16 01:02:48 -0300

    agent seeking food

--- vehicle_food/Colecting food/sketch.js
+++ vehicle_food/Colecting food/sketch.js
@@ -1,5 +1,8 @@
 let vehicle;
-let target;
+let food;
+let isFoodOnScreen = false;
+let foodRadius = 20;
+let distances;
 
 function setup() {
   createCanvas(400, 400);
@@ -7,8 +10,46 @@ function setup() {
 }
 
 function draw() {
+  // background
   background(0);
   fill(255, 0, 0);
   noStroke();
+
+  // food
+  if (isFoodOnScreen) {
+    setFoodPosition();
+    isFoodOnScreen = true;
+  }
+  colide();
+  
+  // seeking food
+  vehicle.seek(food);  
+  
+  // vehicle
   vehicle.show();
+  vehicle.update();
 }
+
+function setFoodPosition() {
+  food = createVector(random(width), random(height));
+  
+  distances = vehicle.distance(food.x, food.y);
+  print(distances[0]);
+  
+  drawFood();
+}
+
+function colide() {
+  if (distances[0] <= foodRadius || distances[1] <= foodRadius || distances[2] <= foodRadius) {
+    isFoodOnScreen = false;
+  } else {
+    drawFood();
+  }
+}
+
+function drawFood() {
+  if (isFoodOnScreen) {
+    circle(food.x, food.y, foodRadius);
+  }
+}
+ 
--- vehicle_food/Colecting food/vehicle.js
+++ vehicle_food/Colecting food/vehicle.js
@@ -3,7 +3,9 @@ class Vehicle {
     this.pos = createVector(x, y);
     this.vel = createVector(0, 0);
     this.acc = createVector(0, 0);
-    this.r = 10;
+    this.r = 5;
+    this.maxSpeed = 4;
+    this.maxForce = 0.25;
   }
   
   show() {
@@ -13,7 +15,50 @@ class Vehicle {
     push();
     translate(this.pos.x, this.pos.y);
     rotate(this.vel.heading());
-    triangle(-this.r, -this.r / 2, -this.r, this.r / 2, this.r, 0);
+    triangle(-this.r, -this.r/2, -this.r, this.r/2, this.r, 0);
+    pop();
+  }
+  
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+    this.edges();
+  }
+  
+  seek(target) {
+    let force = p5.Vector.sub(target, this.pos);
+    force.setMag(this.maxSpeed);
+    force.sub(this.vel);
+    force.limit(this.maxForce);
+    this.applyForce(force);
+  }
+  
+  applyForce(force) {
+    this.acc.add(force);
+  }
+  
+  edges() {
+    // left edge
+    if (this.pos.x > width + this.r) {
+      this.pos.x = -this.r;
+    // right edge
+    } else if (this.pos.x < this.r) {
+      this.pos.x = width + this.r;
+    } 
+    // top edge
+    if (this.pos.y > height + this.r) {
+      this.pos.y = -this.r;
+    // bottom edge
+    } else if (this.pos.y < -this.r) {
+      this.pos.y = height + this.r; 
+    }
+  }
+  
+  // TODO refatorar depois
+  distance(food_x, food_y) {
+    var distances = [dist(-this.r, -this.r/2, food_x, food_y), dist(this.r, this.r/2, food_x, food_y), dist(this.r/2, this.r, food_x, food_y)]
+    return distances;
   }
 }
-// 6:48
